691b3948d3748be9ed81e601752f47fba34e40ef
GakkiSteve <yang335060@qq.com>
2022-12-31 20:15:19 +1300

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div>
+        <i class="iconfont icon-fanhui back-icon"></i>
+      </div>
+    </div>
+    <div class="header-input">
+      <i class="iconfont icon-sousuo"></i>
+    </div>
+    <div class="header-right">
+      城市
+      <i class="iconfont icon-xiajiantou"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="stylus" scoped>
+@import "~@/assets/styles/varibles.styl"
+.header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+        width: .64rem
+        float: left
+        .back-icon
+          text-align: center
+          font-size: .4rem
+          margin-left: .2rem
+    .header-input
+        flex: 1
+        line-height: .64rem
+        margin-top: .12rem
+        margin-left: .2rem
+        margin-bottom: .12rem
+        padding-left: .2rem
+        background: #fff
+        border-radius: .1rem
+        color: #ccc
+
+    .header-right
+        width: 1.24rem
+        float: right
+        text-align: center
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="home">hello world</div>
+  <home-header></home-header>
 </template>
 
 <script>
-// @ is an alias to /src
+//引入组件
 import HelloWorld from "@/components/HelloWorld.vue";
+import HomeHeader from "@/components/Header.vue";
 
 export default {
   name: "Home",
+  //注册组件
   components: {
     HelloWorld,
+    HomeHeader,
   },
 };
 </script>
