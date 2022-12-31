ed23429c37eca71655f1bf133e8b1aa5155cfdae
GakkiSteve <yang335060@qq.com>
2022-12-31 22:10:19 +1300

    change

---
+++ src/components/Swiper.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="wrapper">
+    <swiper :pagination="true" :loop="true" :modules="modules" class="mySwiper">
+      <!-- <swiper-slide>Slide 1</swiper-slide><swiper-slide>Slide 2</swiper-slide
+    ><swiper-slide>Slide 3</swiper-slide><swiper-slide>Slide 4</swiper-slide
+    ><swiper-slide>Slide 5</swiper-slide><swiper-slide>Slide 6</swiper-slide
+    ><swiper-slide>Slide 7</swiper-slide><swiper-slide>Slide 8</swiper-slide
+    ><swiper-slide>Slide 9</swiper-slide> -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+    </swiper>
+  </div>
+  test1
+</template>
+<script>
+import { Swiper, SwiperSlide } from "swiper/vue/swiper-vue.js";
+import "swiper/swiper-bundle.min.css"; // 所有的Swiper样式
+import { Pagination, Navigation } from "swiper";
+
+export default {
+  name: "App",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Pagination, Navigation],
+    };
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true,
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1802/e3/62ce7362ca051d02.jpg_640x200_6db551b7.jpg",
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1801/93/ce59d182aca07102.jpg_640x200_ba03d44c.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper
+  background: #fff
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 31.25%
+  background: #eee
+  .swiper-img
+    width: 100%
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <home-header></home-header>
+  <home-swiper></home-swiper>
 </template>
 
 <script>
 //引入组件
 import HelloWorld from "@/components/HelloWorld.vue";
 import HomeHeader from "@/components/Header.vue";
-
+import HomeSwiper from "@/components/Swiper.vue";
 export default {
   name: "Home",
   //注册组件
   components: {
     HelloWorld,
     HomeHeader,
+    HomeSwiper,
   },
 };
 </script>
