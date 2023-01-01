dfd7c68766a149fc1daf87593ee3770905b35302
GakkiSteve <yang335060@qq.com>
2023-01-02 01:52:18 +1300

    ajax city

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -1,11 +1,12 @@
 <template>
   <city-header></city-header>
   <city-search></city-search>
-  <city-list></city-list>
-  <city-alphabet></city-alphabet>
+  <city-list :cities="cities" :hot="hotCities"></city-list>
+  <city-alphabet :cities="cities"></city-alphabet>
 </template>
 
 <script>
+import axios from "axios";
 import CityHeader from "./CityHeader";
 import CitySearch from "./Search";
 import CityList from "./List";
@@ -17,6 +18,28 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet,
+  },
+  data() {
+    return {
+      cities: {},
+      hotCities: [],
+    };
+  },
+  methods: {
+    getCityInfo() {
+      axios.get("/mock/city.json").then(this.handleGetCityInfoSucc);
+    },
+    handleGetCityInfoSucc(res) {
+      res = res.data;
+      if (res.ret && res.data) {
+        const data = res.data;
+        this.cities = data.cities;
+        this.hotCities = data.hotCities;
+      }
+    },
+  },
+  mounted() {
+    this.getCityInfo();
   },
 };
 </script>
